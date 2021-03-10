63bc500ce484dbb491596d761e0e25b532e8dc3f
Jan Christoph Ebersbach <jceb@e-jc.de>
2021-03-10 13:29:07 +0100

    fix: strip leading v also for predicate comparisons

--- cli.js
+++ cli.js
@@ -10,43 +10,46 @@ const isVersion = (s) => s.match(/^v?[0-9]+(\.[0-9]+)*/) !== null;
 const testArgs = (a) => a.reduce((acc, s) => acc && isVersion(s), true);
 
 const _comp = (validResults) => (a, b) => validResults.includes(cmp(a, b));
+const cmp_predicates = {
+  eq: _comp([0]),
+  ge: _comp([0, 1]),
+  gt: _comp([1]),
+  le: _comp([0, -1]),
+  lt: _comp([-1]),
+};
 
 if (
   ![4, 5].includes(process.argv.length) ||
   !testArgs(process.argv.slice(-2)) ||
   (process.argv.length === 5 &&
-    !["eq", "ge", "gt", "le", "lt"].includes(process.argv[2]))
+    !Object.keys(cmp_predicates).includes(process.argv[2]))
 ) {
   console.log(`Reveived: semver-compare ${process.argv.slice(2).join(" ")}`);
-  console.log("Usage: semver-compare [eq|ge|gt|le|lt] VERSION1 VERSION2");
+  console.log(
+    `Usage: semver-compare [${Object.keys(cmp_predicates).join(
+      "|"
+    )}] VERSION1 VERSION2`
+  );
   console.log("Without predicate:");
   console.log("\texit code 0 if versions are equal");
   console.log("\texit code 1 if first versions is higher");
   console.log("\texit code 2 if first versions is lower");
-  console.log("With predicate [eq|ge|gt|le|lt] :");
-  console.log("\texit code 0 if predicate matches");
-  console.log("\texit code 1 if predicate doesn't match");
+  console.log(`With predicate [${Object.keys(cmp_predicates).join("|")}]:`);
+  console.log("\texit code 0 if predicate applies comparing VERSION1 to VERSION2");
+  console.log("\texit code 1 if predicate doesn't apply");
   console.log("exit code 3 if an error occurs");
   process.exit(3);
 }
 
-let versions = process.argv.slice(-2);
+let versions = process.argv.slice(-2).map(stripV);
 let exitcode = 3;
 
 if (process.argv.length === 4) {
-  exitcode = cmp(...versions.map(stripV));
+  exitcode = cmp(...versions);
   if (exitcode === -1) {
     exitcode = 2;
   }
 } else {
-  exitcode = {
-    gt: _comp([1]),
-    ge: _comp([0, 1]),
-    eq: _comp([0]),
-    le: _comp([0, -1]),
-    lt: _comp([-1]),
-  }[process.argv[2]](...versions)
-    ? 0
-    : 1;
+  exitcode = cmp_predicates[process.argv[2]](...versions) ? 0 : 1;
 }
 process.exit(exitcode);
